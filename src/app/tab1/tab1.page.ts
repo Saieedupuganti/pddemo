45c68cd3df9ff6d6c9ca04fbcd29cafe8e0cdc10
Saieedupuganti <saieedupuganti151@gmail.com>
2023-07-11 23:49:52 +0530

    master page design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab2',
+  selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  isMenuOpen: boolean = false;
 
-  constructor() {}
+  constructor(private menu: MenuController) {}
 
+  ionViewWillEnter() {
+    this.menu.isOpen().then((isOpen) => {
+      this.isMenuOpen = isOpen;
+    });
+  }
+
+  toggleMenu() {
+    this.menu.toggle().then(() => {
+      this.menu.isOpen().then((isOpen) => {
+        this.isMenuOpen = isOpen;
+      });
+    });
+  }
 }
